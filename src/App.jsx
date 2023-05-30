e32e0b762eb8babd62e95223f11fc03de70d4222
juliana <julianapereira.dev@gmail.com>
2023-05-30 16:16:41 -0300

    Consegui fazer a rota de login (faltou colocar animação no button)

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Cadastro from "./pages/Cadastro";
 import Habitos from "./pages/Habitos";
 import Hoje from "./pages/Hoje";
 import Historico from "./pages/Historico";
+import { useState } from "react";
 
 export default function App() {
 
+  const [token, setToken] = useState('');
   
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login setToken={setToken} />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/habitos" element={<Habitos />} />
-        <Route path="/hoje" element={<Hoje />} />
-        <Route path="/historico" element={<Historico />} />
+        <Route path="/habitos" element={<Habitos token={token} />} />
+        <Route path="/hoje" element={<Hoje token={token} />} />
+        <Route path="/historico" element={<Historico token={token} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,73 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../images/logo-trackit.png";
+import { useState } from "react";
+import axios from "axios";
 
-export default function Login() {
+export default function Login(props) {
+
+  const {setToken} = props;
+
+  const navigate = useNavigate();
+
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); 
+  
+  
+  function LoginUser(e) {
+    e.preventDefault();
+
+
+    const URL = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login';
+
+    const novoLogin = {email, password};
+
+    const promise = axios.post(URL, novoLogin);
+
+    setIsDisabled(true)
+
+    promise.then( resposta => {
+      
+      console.log(resposta.data.token);
+      console.log('resposta.data de login:', resposta.data)
+      
+      setToken(resposta.data.token);
+
+      navigate('/hoje');
+
+    });
+
+    promise.catch( erro => {
+      alert(erro.response.data.message)
+      setIsDisabled(false)
+      console.log('ERRO DE LOGIN AQUI:', erro)
+    });
+  }
+
   return (
-    <DivLoginContainer>
+    <DivLoginContainer onSubmit={LoginUser}>
       <img src={logo} />
       <p>TrackIt</p>
       <input 
       type="email" 
       placeholder="email"
-      required 
+      required
+      disabled={isDisabled}
+      value={email}
+      onChange={ (e) => setEmail(e.target.value)}
       />
       <input 
       type="password" 
       placeholder="senha"
-      required 
+      required
+      disabled={isDisabled}
+      value={password}
+      onChange={ (e) => setPassword(e.target.value)} 
       />
+      
       <button type="submit" >Entrar</button>
+
       <Link to={`/cadastro`} style={{ color: "#52b6ff", fontSize: "14px" }}>
         Não tem uma conta? Cadastre-se!
       </Link>
